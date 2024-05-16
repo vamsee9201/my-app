146f7659d5db86632e92cfd69cfff3666ff52b10
vamsee9201 <vamseekrishna9201@gmail.com>
2024-05-15 22:55:44 -0500

    getting to print lists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
+// const user = {
+//   name: 'Hedy Lamarr',
+//   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+//   imageSize: 90,
+// };
 
 // function App() {
 //   return (
@@ -28,22 +28,46 @@ const user = {
 //hello, testing git commit
 
 
-export default function profile() {
- return (
-<>
-<h1>{user.name}</h1>
-<img 
-className='avatar'
-src={user.imageUrl}
-alt={'Photo of ' + user.name}
-style={
-  {
-    width:user.imageSize,
-    height:user.imageSize
-  }
-}
-/>
-</>
- );
+// export default function profile() {
+//  return (
+// <>
+// <h1>{user.name}</h1>
+// <img 
+// className='avatar'
+// src={user.imageUrl}
+// alt={'Photo of ' + user.name}
+// style={
+//   {
+//     width:user.imageSize,
+//     height:user.imageSize
+//   }
+// }
+// />
+// </>
+//  );
+
+// }
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+ const listItems = products.map(product => 
+  <li
+  key={product.id}
+  style={{
+
+    color:product.isFruit ? 'magenta' : 'darkgreen'
+  }}
+  >
+{product.title}
+  </li>
+  )
+return (
+<ul>{listItems}</ul>
+);
 
 }
