27f9d23c25b17c2d73fc84bbd7c62c72d7caecb1
vamsee9201 <vamseekrishna9201@gmail.com>
2024-05-15 22:37:06 -0500

    learned about JSX and passing attributes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 
-export default App;
+// function App() {
+//   return (
+//     <>
+//       <h1>Welcome to my app</h1>
+//       <MyButton/>
+//     </>
+//   );
+// }
+
+// function MyButton() {
+// return (
+//   <button>I'm a button</button>
+// );
+// }
+
+
+// export default App;
 
 //hello, testing git commit
+
+
+export default function profile() {
+ return (
+<>
+<h1>{user.name}</h1>
+<img 
+className='avatar'
+src={user.imageUrl}
+alt={'Photo of ' + user.name}
+style={
+  {
+    width:user.imageSize,
+    height:user.imageSize
+  }
+}
+/>
+</>
+ );
+
+}
