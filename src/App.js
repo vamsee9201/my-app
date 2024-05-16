3b132f38058d96870d0be49d16daeabaa3ff8997
vamsee9201 <vamseekrishna9201@gmail.com>
2024-05-15 23:40:14 -0500

    finished the getting started section from react website

--- src/App.js
+++ src/App.js
@@ -72,26 +72,55 @@ import './App.css';
 
 // }
 
+// import { useState } from 'react';
+
+// export default function MyApp() {
+// return (
+//   <div>
+//     <h1>Counters that update separately</h1>
+//     <MyButton/>
+//     <MyButton/>
+//   </div>
+// );
+// }
+
+// function MyButton() {
+//   const [count,setCount] = useState(0);
+//   function handleClick() {
+//     setCount(count + 1)
+//   }
+//   return (
+//     <button onClick={handleClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
+
+
 import { useState } from 'react';
 
 export default function MyApp() {
-return (
-  <div>
-    <h1>Counters that update separately</h1>
-    <MyButton/>
-    <MyButton/>
-  </div>
-);
+  const[count,setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+   <div>
+    <h1>counters that update together</h1>
+    <MyButton count={count} onClick={handleClick}/>
+    <MyButton count={count} onClick={handleClick}/>
+   </div>
+  )
 }
 
-function MyButton() {
-  const [count,setCount] = useState(0);
-  function handleClick() {
-    setCount(count + 1)
-  }
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
+function MyButton({count, onClick}){
+
+ return(
+  <button onClick={onClick}>
+    Clicked {count} times
+  </button>
+ );
 }
+
